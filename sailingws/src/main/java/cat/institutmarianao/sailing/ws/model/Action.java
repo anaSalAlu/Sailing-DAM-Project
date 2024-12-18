5de25e3a20e79e95a65a6ca0ec1cfdc280588b23
cfgs <cfgs@pc-info-host>
2024-12-18 20:13:15 +0100

    Anotaciones clase Actions hechas.

--- sailingws/src/main/java/cat/institutmarianao/sailing/ws/model/Action.java
+++ sailingws/src/main/java/cat/institutmarianao/sailing/ws/model/Action.java
@@ -3,7 +3,24 @@ package cat.institutmarianao.sailing.ws.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -11,12 +28,13 @@ import lombok.NonNull;
 import lombok.experimental.SuperBuilder;
 
 /* Swagger */
-@Schema(oneOf = { Booking.class, Rescheduling.class, Cancellation.class, Done.class}, discriminatorProperty = "type")
+@Schema(oneOf = { Booking.class, Rescheduling.class, Cancellation.class, Done.class }, discriminatorProperty = "type")
 /* Lombok */
 @Data
 @NoArgsConstructor
 @SuperBuilder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(name = "actions")
 public abstract class Action implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -31,23 +49,44 @@ public abstract class Action implements Serializable {
 		BOOKING, RESCHEDULING, CANCELLATION, DONE
 	}
 
-	/* Lombok */
 	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	@Column(name = "id")
+	@NotEmpty
+	@JsonProperty("id")
 	protected Long id;
 
-	/* Lombok */
 	@NonNull
+	@Enumerated(EnumType.STRING)
+	@Column(name = "type", length = 31)
+	@NotBlank
+	@JsonProperty("type")
 	protected Type type;
 
+	@NonNull
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
+	@JoinColumn(name = "performer_username", referencedColumnName = "username")
+	@NotBlank
+	@JsonProperty("performer_username")
 	protected User performer;
 
+	@NonNull
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "date")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@JsonProperty("date")
 	protected Date date = new Date();
 
+	@NonNull
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
+	@JoinColumn(name = "trip_id", referencedColumnName = "id")
+	@NotBlank
+	@JsonProperty("trip")
 	protected Trip trip;
 
-	protected Long idTrip;
-	
 	public String getInfo() {
 		return "";
 	}
+
 }
